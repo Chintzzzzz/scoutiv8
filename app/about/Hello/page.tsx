459af12db080a9ff16b0026b361ce376b00ed392
Chintzzzzz <chirubn03@gmail.com>
2025-09-19 18:44:13 +0100

    Oh i added some font bg

--- app/about/Hello/page.tsx
+++ app/about/Hello/page.tsx
@@ -1,5 +1,8 @@
 export default function Aboutpage(){
     return (
-        <>This is hello page</>
+        <>This is hello page
+        <div className="text-8xl bg-yellow-400 font-bold">JSR</div>
+        </>
+    
     )
 }
